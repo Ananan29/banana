075047d9656d7c9efc18f4aa943466a25232adb3
Ananya Arora <ananyaarora@Ananyas-MacBook-Air.local>
2026-08-01 07:13:15 +0530

    bettered css

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
-const Navbar = ({ Signupclick,LoggedIn }) => {
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import logo from "./../../assets/_ (3).jpeg";
+const Navbar = ({ ShowAuth,LoggedIn }) => {
   return (
-    <div>
+    <div className="navbar">
         <nav>
-            <Link to="/discover">Discover</Link>
-            <Link to="/library">Library</Link>
-            {!LoggedIn && <button onClick={Signupclick}>Sign up</button>}
-            {LoggedIn &&
-              <div>
-                <Link to="/wishlist">Wishlist</Link>
-                <Link to="/cart">Cart</Link>
-                <Link to="/profile">Profile</Link>
+          <div className="nav-left">
+            <img src={logo} className="navbar-logo" alt="logo"/>
+          </div>
+          <div className="nav-center">
+            <NavLink to="/discover">Discover</NavLink>
+            <NavLink to="/library">Library</NavLink>
+          </div>
+          <div className="nav-right">
+            {!LoggedIn?(
+              <button onClick={ShowAuth}>Sign up</button>
+            ):(
+              <div className="navbar-loggedin-pages">
+                <NavLink to="/wishlist">Wishlist</NavLink>
+                <NavLink to="/cart">Cart</NavLink>
+                <NavLink to="/profile">Profile</NavLink>
               </div>
-            }
+            )}
+            </div>
         </nav>
     </div>
   )
